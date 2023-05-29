b073ea470c61f345bdbb72452c97fa23bc683212
Maria <91966309+Mitohon@users.noreply.github.com>
2023-05-29 22:22:50 +0300

    Update gauss3.py

--- gauss3.py
+++ gauss3.py
@@ -1,5 +1,4 @@
-import numpy
-from numpy import array, concatenate
+import numpy as np
 from numpy.linalg import norm
 from numpy.linalg import solve as solve_out_of_the_box
 from numpy import vectorize
@@ -13,53 +12,47 @@ def vect_mult():
     return vectorize(multiply)
 
 def gauss(a, b):
-    ab = concatenate((a, array([b]).T), axis=1) # concatenate заодно и скопирует 
+    ab = np.concatenate((a, np.array([b]).T), axis=1) # concatenate заодно и скопирует 
     d = len(ab)  # размер по старшему измерению
     for i in range(d):
-        for j in range(len(a[i])):
-            if ab[i][j] != 0:
-                print(ab[i][j])
-                r = vectorize(multiply)
-                print(f'multiply to get 1')
-                ab[i] = r (ab[i], 1/ab[i][j])
-                if i != 0:
-                    for t in range(j):
-                        print('subtract from upper rows')
-                        ab[i-1-t] = list(map(add, ab[i-1-t], ab[i]*(-ab[i-1-t][j])))
-                break
+        j = i
+        while j < len(a[i]) and ab[i][j] == 0:
+            j += 1
+        if j == len(a[i]):
+            continue
+        r = vectorize(multiply)
+        ab[i] = r (ab[i], 1/ab[i][j])
         for k in range(i+1, d):
-            print(f'subtract {i}-th row')
             ab[k] = ab[k] - ab[k][j]*ab[i]
-        print(ab)
        
-
-#проверка на совместимость 
+    #проверка на совместимость 
     for i in range(d):            
         nonzero_r = False
         for j in range(len(a[i])):
-            if abs(ab[i][j]) > 10:
+            if abs(ab[i][j]) > 10**(-10):
                 nonzero_r = True
-        
-        if nonzero_r == False and abs(ab[i][-1]) > 10:
+        if nonzero_r == False and abs(ab[i][-1]) > 10**(-10):
             return 'решений нет'
     
-    solution = len(ab)*[0]         
-    for i in range(len(ab)):
-        for j in range(len(ab[i])-1):
-            if ab[i][j] == 1:
-                solution[j] = ab[i][-1]
+    solution = [0]*len(ab)
+    for i in range(len(ab)-1, -1, -1):
+        res = ab[i][-1]
+        for j in range(len(ab[i])-2, i-1, -1):
+            res -= ab[i][j]*solution[j]
+        if ab[i][i] != 0:
+            solution[i] = res / ab[i][i]
     return solution
 
 
-a = array([
+a = np.array([
     [0, 2, 1, 3],
     [1, 1, 1, 1],
     [0, 4, 5, 7],
     [2, 2, 2, 2]
 ], dtype=float)
 
-b = array([5, 6, 7, 12], dtype=float)
+b = np.array([5, 6, 7, 12], dtype=float)
 
-print(concatenate((a, array([b]).T), axis=1))
+print(np.concatenate((a, np.array([b]).T), axis=1))
 print("\nРешения системы:")
 print(gauss(a,b))
